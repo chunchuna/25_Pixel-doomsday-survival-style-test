2d5dfb45b34fa663db33974dbf01668dc9289816
chunchun <578806739@qq.com>
2025-05-03 12:43:22 +0800

    优化engine代码

---
+++ scripts/eventsheet.ts
@@ -0,0 +1,3 @@
+import { pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit } from "./engine.js";
+var gl$_run_eventhandle_ =pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit.gl$_run_eventhandle_;
+
