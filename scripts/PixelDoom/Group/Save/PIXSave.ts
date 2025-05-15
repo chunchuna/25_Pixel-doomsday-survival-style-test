f1d2d7cb6b110d4ed5e426f1df8ca4bc5ba5ea95
chunchuna <you@example.com>
2025-05-15 22:18:18 +0800

    重新修改了存档的代码

--- scripts/PixelDoom/Group/Save/PIXSave.ts
+++ scripts/PixelDoom/Group/Save/PIXSave.ts
@@ -1,19 +1,29 @@
 import { pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit } from "../../../engine.js";
 
+
+pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit.gl$_ubu_init(()=>{
+    pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit.RUN_TIME_.addEventListener("save",(e)=>{
+        alert("保存了游戏")
+    })
+    pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit.RUN_TIME_.addEventListener("load",(e)=>{
+        alert("读取了游戏")
+    })
+})
+
+
 pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit.gl$_ubu_init(() => {
    
     
     document.addEventListener('keydown', (event: KeyboardEvent) => {
         // 示例检测
         if (event.key === 'p') {
-            alert("保存")
             Save.SaveGame("test2")
         }
 
         if (event.key === 'o') {
-            alert("读取")
             Save.LoadGame("test2")
         }
+
     });
 
 })
@@ -29,8 +39,8 @@ export class Save {
         pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit.RUN_TIME_.callFunction("LoadGame", Slot)
     }
 
-    static LoadGameFromJson() {
-        pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit.RUN_TIME_.callFunction("LoadGameByJson")
+    static LoadGameFromJson(Json:string) {
+        pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit.RUN_TIME_.callFunction("LoadGameByJson",Json)
 
     }
 
