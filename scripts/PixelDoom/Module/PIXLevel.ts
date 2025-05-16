0af53b955fe46ca41b901f9a0460411b2e79aed4
纯纯 <xiangjunjie@libii.com>
2025-05-16 18:14:47 +0800

    设置Level整体的缩放效果

--- scripts/PixelDoom/Module/PIXLevel.ts
+++ scripts/PixelDoom/Module/PIXLevel.ts
@@ -1,5 +1,17 @@
 import { pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit } from "../../engine.js";
+var CameraZoomValue: number = 0.5;
+var CameraZoomTarget = 0.5;
 
-pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit.gl$_ubu_init(()=>{
+pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit.gl$_ubu_init(() => {
+    // 初始化时设置初始值
+    CameraZoomValue = 0.5;
+    pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit.RUN_TIME_.layout.scale = CameraZoomValue;
+})
 
+// 取消注释update函数，在每帧更新中执行Lerp
+pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit.gl$_ubu_update(() => {
+    // 使用较小的插值系数(0.05)来实现平滑过渡
+    CameraZoomValue = pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit.Justlerp(CameraZoomValue, CameraZoomTarget, 0.0005)
+    console.log(CameraZoomValue)
+    pmlsdk$ProceduralStorytellingSandboxRPGDevelopmentToolkit.RUN_TIME_.layout.scale = CameraZoomValue;
 })
